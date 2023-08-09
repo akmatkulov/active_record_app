eb6cb96c3b3343c1c6d69d6183e8592a43b5d021
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2023-08-09 18:21:51 +0600

    Add length validation

--- app.rb
+++ app.rb
@@ -7,6 +7,11 @@ require 'sinatra/activerecord'
 set :database, {adapter: "sqlite3", database: "barbershop.db"}
 
 class Client < ActiveRecord::Base
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :phone, presence: true
+  validates :date_stamp, presence: true
+  validates :barber, presence: true
+  validates :color, presence: true
 end
 
 class Barber < ActiveRecord::Base
@@ -15,19 +20,23 @@ end
 before do
   @barbers = Barber.all
 end
-
 get '/' do 
   @barbers = Barber.order 'created_at DESC'
   erb :index
 end 
 
 get '/visit' do
+  @users = Client.new
   erb :visit
 end
 
 # Отправить данные
 post '/visit' do
-  users = Client.new params[:client]
-  users.save
-  erb :visit
+  @users = Client.new params[:client]
+  if @users.save
+    erb "<h2>Вы записались</h2>"
+  else
+    @error = @users.errors.full_messages.first
+    erb :visit
+  end
 end
