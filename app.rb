a8d60be33310c21c7e3b02312bc532799b0ab006
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2023-08-09 15:25:52 +0600

    Save client information lame way

--- app.rb
+++ app.rb
@@ -12,8 +12,28 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+  @barbers = Barber.all
+end
+
 get '/' do 
   @barbers = Barber.order 'created_at DESC'
   erb :index
 end 
 
+get '/visit' do
+  erb :visit
+end
+
+# Отправить данные
+post '/visit' do
+  users = Client.new
+  users.name = params[:name]
+  users.phone = params[:phone_number]
+  users.date_stamp = params[:date_time]
+  users.barber = params[:barber]
+  users.color = params[:color]
+  users.save
+
+  erb :visit
+end
